2b02d3aa3711772fa56851846569bb18ad2ddc3f
andrei <a.moldoveanu@alkemytech.it>
2018-05-11 14:13:33 +0200

    first commit

---
+++ src/main/java/org/mitre/web/HomeController.java
@@ -0,0 +1,75 @@
+/*******************************************************************************
+ * Copyright 2017 The MIT Internet Trust Consortium
+ *
+ * Portions copyright 2011-2013 The MITRE Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package org.mitre.web;
+
+import java.security.Principal;
+import java.util.Locale;
+import java.util.Set;
+
+import javax.annotation.Resource;
+
+import org.mitre.openid.connect.client.OIDCAuthenticationFilter;
+import org.mitre.openid.connect.client.SubjectIssuerGrantedAuthority;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+@RequestMapping("/escape")
+public class HomeController {
+
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+//	@RequestMapping(value = "/", method = RequestMethod.GET)
+//	public String home(Locale locale, Model model, Principal p) {
+//		return "home";
+//	}
+//
+//	@RequestMapping("/open")
+//	public String open(Principal p) {
+//		return "open";
+//	}
+//
+//	@RequestMapping("/admin")
+//	@PreAuthorize("hasRole('ROLE_ADMIN')")
+//	public String admin(Model model, Principal p) {
+//
+//
+//		return "admin";
+//	}
+
+	@RequestMapping(value="/text", method= RequestMethod.GET)
+	public String text(
+			@RequestParam(value="text") String text) {
+		
+		return "text returned";
+	}
+
+}
